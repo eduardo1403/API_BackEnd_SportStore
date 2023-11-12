3bb6251bd410e527b80937a5cc5fdf4db249e9d4
eduardo1403 <eduardo1403hm@gmail.com>
2023-11-12 00:51:33 -0600

    feat - Obtener todos los productos

--- src/routes/product.js
+++ src/routes/product.js
@@ -3,6 +3,11 @@ const Product = require('../models/product');
 
 const router = express.Router();
 
+//Obtener todas los productos
+router.get("/product", (req, res) => {
+    Product.find().then((data) => res.json(data)).catch((error) => res.json({message: error}))
+});
+
 //Obtener un producto por id
 router.get("/product/:id", (req, res) => {
     const {id} = req.params;
